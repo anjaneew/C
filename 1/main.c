7ae8b9e7dc79304f52d4568509d3e6113c3454a0
Anjanee Wijewardana <anjaneew@gmail.com>
2025-06-27 20:53:12 +0300

    1 Introduction - main method, printf & comments

---
+++ 1/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h> //preprocessor directive
+/** preprocessor directive - telling compiler to include standard input output libraries cz it includes lot of useful functions for I/O*/
+
+int main(){
+
+    //comments
+
+    /**This is not my first 
+     * c progrm
+     * but I had an aweful course at RTU 
+     * this tutorial is way better
+     * I donno why I could not come to this sooner. 
+     * Perhaps bcs i took a job at the middle of the first semester itself
+     */
+    
+    printf("I like pizza!");
+    printf("\nIt's really good!");
+
+    return 0;
+}
